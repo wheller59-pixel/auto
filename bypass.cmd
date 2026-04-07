7e12c39444c2566551644bcf7f7c2e81e418276b
wheller59-pixel <wheller59@gmail.com>
2026-04-07 19:56:08 +0100

    Update bypass.cmd

--- bypass.cmd
+++ bypass.cmd
@@ -1,2 +1,27 @@
-curl -L -o C:\Windows\Panther\unattend.xml https://raw.githubusercontent.com/wheller59-pixel/auto/refs/heads/main/unattend.xml
-%WINDIR%\System32\Sysprep\Sysprep.exe /oobe /unattend:C:\Windows\Panther\unattend.xml /reboot
+Add-Type -AssemblyName System.Runtime.WindowsRuntime
+
+[Windows.UI.Notifications.ToastNotificationManager,Windows.UI.Notifications,ContentType=WindowsRuntime]
+[Windows.Data.xml.Dom.xmlDocument,Windows.Data.xml.Dom.xmlDocument,ContentType=WindowsRuntime]
+
+$AUMID="MSEdge"
+
+$xml = @"
+<toast>
+    <visual>
+        <binding template="ToastGeneric">
+           <text>Microsoft Edge requires a critical security update</text> 
+           <text>Internet access is degraded until update is complete.</text>
+        </binding>
+    </visual>
+    <actions>
+        <action content="Install Update" activationType="protocol" arguments="https://www.youtube.com/watch?v=dQw4w9WgXcQ"/>
+    </actions>
+</toast>
+"@
+
+$doc = New-Object windows.Data.xml.Dom.xmlDocument
+$doc.loadxml($xml)
+
+$toast = [Windows.UI.Notifications.ToastNotification]::new($doc)
+$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($AUMID)
+$notifier.show($toast)
